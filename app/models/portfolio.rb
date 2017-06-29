deda3876056c64bb0107b755ade28ec893ec5712
Jordan Hudgens <jordan@devcamp.com>
2017-06-29 19:50:53 +0000

    Merge errors

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,8 +1,7 @@
 class Portfolio < ApplicationRecord
-<<<<<<< HEAD
-<<<<<<< HEAD
-    validates_presence_of :title, :body, :main_image, :thumb_image
-=======
+
+  validates_presence_of :title, :body, :main_image, :thumb_image
+
   has_many :technologies
   accepts_nested_attributes_for :technologies,
                                 reject_if: lambda { |attrs| attrs['name'].blank? }
@@ -22,8 +21,5 @@ class Portfolio < ApplicationRecord
     self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
     self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
   end
->>>>>>> 9d9375d4f94247d5aa5515154ad5610052c3646c
-=======
-  validates_presence_of :title, :body, :main_image, :thumb_image
->>>>>>> de56027a934b4ee4efa95f38d5e53232af803044
+
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,8 +1,7 @@
 class Skill < ApplicationRecord
-<<<<<<< HEAD
-<<<<<<< HEAD
-    validates_presence_of :title, :percent_utilized
-=======
+
+  validates_presence_of :title, :percent_utilized
+
   include Placeholder
   validates_presence_of :title, :percent_utilized
   
@@ -11,8 +10,5 @@ class Skill < ApplicationRecord
   def set_defaults
     self.badge ||= Placeholder.image_generator(height: '250', width: '250')
   end
->>>>>>> 9d9375d4f94247d5aa5515154ad5610052c3646c
-=======
-  validates_presence_of :title, :percent_utilized
->>>>>>> de56027a934b4ee4efa95f38d5e53232af803044
+
 end
